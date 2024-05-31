4b76a7aaa99ff4b5e0c98189a854f0063c9814e8
Sema Nur EKMEKCİ <ekmekci.semanur@gmail.com>
2024-06-01 01:06:42 +0300

    Add files via upload

---
+++ led_control/arduino_led_control_csharp/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace Project_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.comboBoxPortName = new System.Windows.Forms.ComboBox();
+            this.comboBoxBaudRate = new System.Windows.Forms.ComboBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.button1.Location = new System.Drawing.Point(52, 157);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(157, 58);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "LED ON";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.button2.Location = new System.Drawing.Point(305, 157);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(151, 58);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "LED OFF";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(49, 234);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // comboBoxPortName
+            // 
+            this.comboBoxPortName.FormattingEnabled = true;
+            this.comboBoxPortName.Location = new System.Drawing.Point(53, 30);
+            this.comboBoxPortName.Name = "comboBoxPortName";
+            this.comboBoxPortName.Size = new System.Drawing.Size(145, 30);
+            this.comboBoxPortName.TabIndex = 3;
+            this.comboBoxPortName.SelectedIndexChanged += new System.EventHandler(this.comboBoxPortName_SelectedIndexChanged);
+            // 
+            // comboBoxBaudRate
+            // 
+            this.comboBoxBaudRate.FormattingEnabled = true;
+            this.comboBoxBaudRate.Items.AddRange(new object[] {
+            "9600"});
+            this.comboBoxBaudRate.Location = new System.Drawing.Point(280, 30);
+            this.comboBoxBaudRate.Name = "comboBoxBaudRate";
+            this.comboBoxBaudRate.Size = new System.Drawing.Size(149, 30);
+            this.comboBoxBaudRate.TabIndex = 4;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(83, 91);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(105, 40);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Port On";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(314, 91);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(115, 40);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Port Off";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(594, 270);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.comboBoxBaudRate);
+            this.Controls.Add(this.comboBoxPortName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.ComboBox comboBoxPortName;
+        private System.Windows.Forms.ComboBox comboBoxBaudRate;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ led_control/arduino_led_control_csharp/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+
+namespace Project_1
+{
+    public partial class Form1 : Form
+    {
+        int sema;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SerialPort sr = new SerialPort();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] portNames = SerialPort.GetPortNames();
+            comboBoxPortName.Items.AddRange(portNames);
+            
+            
+            //serialPort1.PortName = "COM5";
+            //serialPort1.BaudRate = 9600;
+            //serialPort1.Open();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            sr.Write("1");
+            label1.Text = "LED ON";
+            button1.Enabled = false;
+            button2.Enabled = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            sr.Write("0");
+            label1.Text = "LED OFF";
+            button1.Enabled = true;
+            button2.Enabled = false;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if(sr.IsOpen == true)
+            {
+                sr.Close();
+            }
+        }
+
+        private void comboBoxPortName_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            sr.PortName = Convert.ToString(comboBoxPortName.Text);
+           
+        }
+
+        private void comboBoxBaudRate_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            sr.BaudRate = Convert.ToInt32(comboBoxBaudRate.Text);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (sr.IsOpen)
+            {
+                sr.Close();
+                label1.Text = "Port ON";
+                sr.Open();
+            }
+            else
+            {
+                sr.Open();
+                label1.Text = "Port ON";
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (sr.IsOpen)
+            {
+                sr.Close();
+                label1.Text = "Port OFF";
+                
+            }
+            else
+            {
+                sr.Close();
+                label1.Text = "Port OFF";
+            }
+        }
+    }
+}
